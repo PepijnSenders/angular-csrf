dc95cef8ad354692ca1273bde3335481acd1b1b5
Pepijn Senders <p.senders92@gmail.com>
2014-10-31 14:51:36 +0100

    unfunctional

--- dist/angular-csrf.js
+++ dist/angular-csrf.js
@@ -1,14 +1,12 @@
 var csrfApp = angular.module('csrfApp', []);
 csrfApp.factory('csrfToken', function() {
 
-  var getCsrfToken = function() {
-    var metaTags = document.getElementsByTagName('meta');
+  var metaTags = document.getElementsByTagName('meta');
 
-    for (var i = 0; i < metaTags.length; i++) {
-      if (metaTags[i].getAttribute('name') === 'csrf') {
-        return metaTags[i].getAttribute('content');
-      }
+  for (var i = 0; i < metaTags.length; i++) {
+    if (metaTags[i].getAttribute('name') === 'csrf') {
+      return metaTags[i].getAttribute('content');
     }
-  };
+  }
 
 });
